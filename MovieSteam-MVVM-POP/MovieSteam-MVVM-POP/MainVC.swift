9a3eb7882e1d3c584077f8e99f7827e950fdc13c
Burak Altunoluk <63430143+BurakAltunoluk@users.noreply.github.com>
2022-09-22 13:19:26 +0100

    first upload

---
+++ MovieSteam-MVVM-POP/MovieSteam-MVVM-POP/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieSteam-MVVM-POP
+//
+//  Created by Burak Altunoluk on 21/09/2022.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
